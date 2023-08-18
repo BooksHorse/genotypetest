95c57347ca737428012daa87475939dd2d40792c
mochi <bookshorse@outlook.jp>
2023-08-18 02:27:13 +0000

    initial commit

--- main.cs
+++ main.cs
@@ -2,21 +2,39 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+		var a = new Genotype("aa");
+		var b = new Genotype("AA");
+		var c = a+b;
+    Console.WriteLine (c.GetAllele());
   }
 }
 class Genotype {
-	public string genotype;
+	public string Alleles;
 	public static Generation operator +(Genotype f1, Genotype f2) 
    {
-      var foo = // some logic to generate a result
-      return foo;
+      var generation = new Generation(f1,f2);
+      return generation;
 		 }
+	public Genotype(string alleles) {
+		Alleles=alleles;
+	}
+	public override string ToString()
+    {
+        return Alleles;
+    }
 }
 class Generation {
 	public string First;
 	public string Second;
 	public Generation(Genotype f1,Genotype f2) {
-		
+		First=f1.Alleles;
+		Second=f2.Alleles;
 	}
+	public char[] GetAllele() {
+		return (First+Second).ToCharArray();
+	}
+	public override string ToString()
+    {
+        return First+Second;
+			}
 }
